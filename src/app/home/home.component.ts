ad03016c66e7e8c296091c86a567d41e92965add
Ismaeel Abuzahra <69389579+Ismaeel77@users.noreply.github.com>
2026-01-13 19:43:45 +0000

    update num of cart items method

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,9 +11,12 @@ export class HomeComponent implements OnInit {
   constructor(private _ProductsService: ProductsService, private _CartService:CartService) {}
   allProducts: any[] = [];
   searchTerm:string =''
+  cartItems:number = 0;
   ngOnInit(): void {
     this._ProductsService.getProducts().subscribe({
       next: (response) => {
+        console.log(response);
+        
         this.allProducts = response.data;
       },
 
@@ -22,7 +25,10 @@ export class HomeComponent implements OnInit {
 
   addToCart(productId:string) {
     this._CartService.addProductToCart(productId).subscribe({
-      next: (response) => {console.log(response);},
+      next: (response) => {
+        console.log(response);
+        this._CartService.numberOfCartItems.next(response.numOfCartItems)
+      },
       error: (err) => {console.log(err);}
     })
   }
